82d6d06cc0f5f8f0fecdc1ffae3d93e4cba1f019
David Colarusso <colarusso@users.noreply.github.com>
2024-05-30 11:16:38 -0400

    copy edits

--- js/reader.js
+++ js/reader.js
@@ -69,9 +69,9 @@ document.addEventListener('DOMContentLoaded', () => {
         
         Make sure you have a file name specified in the url. The URL should end with something that looks like this: <i style="white-space: nowrap;">?file=file_name.txt</i>`;
         text = `<b>Selected Works of Edgar Allan Poe</b>
-
-        <i>You're position will be remembered for this browser within each of the texts below.</i>
         
+        <i>This browser should remember you're position in each of the following. To navigate to their beginnings or return here, use the ☰ menu. Use ⛭ to enable dark mode or change fonts.</i>        
+
         <a href="?file=texts/The Purloined Letter.txt">The Purloined Letter</a>
         
         <a href="?file=texts/The Gold-Bug.txt">The Gold-Bug</a>`;
