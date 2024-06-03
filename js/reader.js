3d34c60d207efc8bb9f13cf71629e0008fc3bb1d
David Colarusso <colarusso@users.noreply.github.com>
2024-06-03 17:58:08 -0400

    parameter change

--- js/reader.js
+++ js/reader.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    //screen.orientation.lock("natural")
+
     window.onresize = function(){ location.reload(); }
     
     const content = document.getElementById('content');
@@ -462,7 +464,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
-    window.toggleInfo = function(focus=0) {
+    window.toggleInfo = function(focus_call=0) {
         stop_talk();
         document.getElementById("jumpto").value = ((currentPage / (pages.length - 1)) * 100);
         if (information.style.display === "none" || information.style.display === "") {
@@ -471,7 +473,7 @@ document.addEventListener('DOMContentLoaded', () => {
             information.style.display = "none";
         }
         document.getElementById("infoToggle").blur();
-        if (focus==1) {
+        if (focus_call==1) {
             document.getElementById('jumpto').focus();
         }
     }
@@ -486,7 +488,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    window.toggleSettings = function(focus=0) {
+    window.toggleSettings = function(focus_call=0) {
         stop_talk();
         if (controls.style.display === "none" || controls.style.display === "") {
             controls.style.display = "flex";
@@ -496,7 +498,7 @@ document.addEventListener('DOMContentLoaded', () => {
             applySettings();
         }
         document.getElementById("infoToggle").blur();
-        if (focus==1) {
+        if (focus_call==1) {
             document.getElementById('fontType').focus();
         }
     }
